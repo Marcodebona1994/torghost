50381c27cfd4d4caeabb73ec8a21227bb47c29a2
Marco De Bona <marcodebona1994@gmail.com>
2022-02-01 01:00:09 +0100

    change build folder

--- installer.sh
+++ installer.sh
@@ -1,10 +1,10 @@
 echo "Torghost installer v3.0"
 echo "Installing prerequisites "
-sudo apt-get install tor python3-pip cython3 -y 
+sudo apt-get install tor python3-pip cython3 -y
 echo "Installing dependencies "
 sudo pip3 install -r requirements.txt
-mkdir build
-cd build
+mkdir -p bin
+cd bin
 py3_version=$(python3 -V | cut -d' ' -f2 | cut -d'.' -f1,2)
 if [ $? -eq 0 ]; then
 	echo [SUCCESS] Python ${py3_version} installed
@@ -12,21 +12,21 @@ else
 	echo [ERROR] Python3 version not found
 	exit 1
 fi
-cython3 ../torghost.py --embed -o torghost.c --verbose
+cython3 torghost.py --embed -o bin/torghost.c --verbose
 if [ $? -eq 0 ]; then
     echo [SUCCESS] Generated C code
 else
     echo [ERROR] Build failed. Unable to generate C code using cython3
     exit 1
 fi
-gcc -Os -I /usr/include/python${py3_version} -o torghost torghost.c -lpython${py3_version} -lpthread -lm -lutil -ldl
+gcc -Os -I /usr/include/python${py3_version} -o bin/torghost bin/torghost.c -lpython${py3_version} -lpthread -lm -lutil -ldl
 if [ $? -eq 0 ]; then
     echo [SUCCESS] Compiled to static binary
 else
     echo [ERROR] Build failed
     exit 1
 fi
-sudo cp -r torghost /usr/bin/
+sudo cp -r bin/torghost /usr/bin/
 if [ $? -eq 0 ]; then
     echo [SUCCESS] Copied binary to /usr/bin
 else
