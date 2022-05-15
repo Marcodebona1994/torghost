f5f5e568f3989c6a4067a2ca317be2c5c5e29a72
Marcodebona1994 <marcodebona1994@gmail.com>
2022-05-15 14:54:36 -0400

    Fix iptables script

--- conf/iptables_setup.sh
+++ conf/iptables_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NON_TOR="192.168.0.0/16 172.16.0.0/12"
+NON_TOR="192.168.0.0/16 172.16.0.0/12 127.0.0.0/8 10.0.0.0/8"
 TOR_UID=$(id -ur debian-tor)
 TRANS_PORT="9040"
 
@@ -9,14 +9,13 @@ iptables -t nat -F
 
 iptables -t nat -A OUTPUT -m owner --uid-owner $TOR_UID -j RETURN
 iptables -t nat -A OUTPUT -p udp --dport 53 -j REDIRECT --to-ports 5353
-for NET in $NON_TOR 127.0.0.0/9 127.128.0.0/10; do
+iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+for NET in $NON_TOR; do
  iptables -t nat -A OUTPUT -d $NET -j RETURN
+ iptables -A OUTPUT -d $NET -j ACCEPT
 done
 iptables -t nat -A OUTPUT -p tcp --syn -j REDIRECT --to-ports $TRANS_PORT
 
-iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-for NET in $NON_TOR 127.0.0.0/8; do
- iptables -A OUTPUT -d $NET -j ACCEPT
-done
 iptables -A OUTPUT -m owner --uid-owner $TOR_UID -j ACCEPT
 iptables -A OUTPUT -j REJECT
